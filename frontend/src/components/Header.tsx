0cac7fdc49db8e83b0761bf2df53271039bf513a
Sofiia13 <sophiyastanish@gmail.com>
2025-09-30 21:05:31 +0300

    add router

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,28 @@
 import type React from "react";
+import { Link } from "react-router-dom";
 
 export const Header: React.FC = () => {
   return (
     <header className="w-full mx-auto bg-[#bae8e8] text-[#272343] shadow-md rounded-4xl">
       <nav className="flex justify-between items-center px-6 py-4">
-        <h1 className="text-xl font-bold">Algorithms Visualizer</h1>
+        <Link to="/">
+          <h1 className="text-xl font-bold">Algorithms Visualizer</h1>
+        </Link>
+
         <ul className="flex gap-6">
-          <li className="cursor-pointer hover:text-[#f9bc60] transition-colors">
+          <Link
+            to="/sorting"
+            className="cursor-pointer hover:text-[#f9bc60] transition-colors"
+          >
             Sorting Algorithms
-          </li>
-          <li className="cursor-pointer hover:text-[#f9bc60] transition-colors">
-            Graphs
+          </Link>
+          <li>
+            <Link
+              to="/data-structures"
+              className="cursor-pointer hover:text-[#f9bc60] transition-colors"
+            >
+              Data Structures
+            </Link>
           </li>
         </ul>
       </nav>
