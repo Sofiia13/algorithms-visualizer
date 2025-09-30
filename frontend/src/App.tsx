1df0a0f48f9521b7a06faaaabfe1f2c824107b92
Sofiia13 <sophiyastanish@gmail.com>
2025-09-30 19:07:55 +0300

    add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { ArrayGenerator } from "./components/ArrayGenerator";
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className="bg-[#fffffe] min-h-screen px-50 py-10">
+      <Header />
       <ArrayGenerator />
-    </>
+    </div>
   );
 }
 
